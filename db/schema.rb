47e5a6a1fafa6f9edfa8a52eed6a38589a2a8b2f
playpool513 <playpool513@gmail.com>
2014-08-06 11:53:09 +0800

    $ bundle exec rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140806032119) do
+
+  create_table "cafes", force: true do |t|
+    t.string   "name"
+    t.text     "address"
+    t.string   "tel"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
