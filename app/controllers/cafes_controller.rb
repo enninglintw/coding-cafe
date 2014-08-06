ea3952639a7e2eb26f1f965f2c49f3da15361aee
playpool513 <playpool513@gmail.com>
2014-08-06 15:17:21 +0800

    def index in cafes_controller.rb

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,2 +1,39 @@
 class CafesController < ApplicationController
+
+
+  def index
+    @cafes = Cafe.all
+  end
+
+
+  def new
+    
+  end
+
+
+  def show
+    
+  end
+
+
+  def edit
+    
+  end
+
+
+  def create
+    
+  end
+
+
+  def update
+    
+  end
+
+
+  def destroy
+    
+  end
+
+
 end
